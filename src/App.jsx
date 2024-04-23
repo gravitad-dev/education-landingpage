0bd2cea2e09bbca4548566ae6bd035dec20b040f
silvak <silvak.jeg@gmail.com>
2024-04-23 19:39:14 -0400

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import { Button } from "@/components/ui/button";
+import Layout from "@/components/Layout";
+import Hero from "@/components/section/Hero";
+import HeroCarousel from "@/components/HeroCarousel";
+import data from "./data.json";
 
 function App() {
+  const { slides } = data.section1;
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <body>
+      <Layout>
+        <Hero />
+      </Layout>
+      //
+      <section className="bg-[url(/bgData.png)]  w-full h-[162px] relative">
+        <div className="absolute right-[0] top-[-62px] w-[62px] overflow-hidden">
+          <div className=" w-[124px] h-[124px] bg-[#FFCF59] rounded-full "></div>
+        </div>
+        <div className="absolute left-[0] bottom-[-30px] w-[82px] overflow-hidden rotate-180">
+          <div className=" w-[124px] h-[124px] bg-[#FFCF59] rounded-full "></div>
+        </div>
+        <HeroCarousel slides={slides} />
+      </section>
+    </body>
   );
 }
 
